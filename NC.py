1ae4ed77d2071ce897976ce89b9a2517c17c4aab
ASzklar <111814288+ASzklar@users.noreply.github.com>
2024-02-22 17:03:49 -0300

    Streamlit

--- NC.py
+++ NC.py
@@ -2,25 +2,25 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('fraudes_limpio.csv')
+#df = pd.read_csv('fraudes_limpio.csv')
 
 st.title("Probando Streamlit :hand:")
 st.text("No Country Proyecto detección de fraudes")
 st.image("fraude_imagen.png")
 
-st.radio("Elige una opción", df.columns)
+#st.radio("Elige una opción", df.columns)
 
-fraudes = df[df['isFraud'] == 1]
+#fraudes = df[df['isFraud'] == 1]
 
-fraudes_por_tipo = fraudes.groupby('type').size()
+#fraudes_por_tipo = fraudes.groupby('type').size()
 st.write("***Fraudes***")
-fig, ax = plt.subplots()
-fraudes_por_tipo.plot(kind='bar', ax=ax)
-ax.set_title('Fraudes por tipo de transacción')
-ax.set_xlabel('Tipo de transacción')
-ax.set_ylabel('Cantidad')
-st.pyplot(fig)
+#fig, ax = plt.subplots()
+#fraudes_por_tipo.plot(kind='bar', ax=ax)
+#ax.set_title('Fraudes por tipo de transacción')
+#ax.set_xlabel('Tipo de transacción')
+#ax.set_ylabel('Cantidad')
+#st.pyplot(fig)
 
-st.dataframe(df.head())
+#st.dataframe(df.head())
 
 #@st.cache
